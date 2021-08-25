f15d08dbdcc0d6b7e1f87551e28d37cc03a4c779
J-Whisler <jwhisler521@gmail.com>
2021-08-25 18:50:05 -0400

    Navbar component created and rendered to App.js

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>Logo</div>
+      <div>Navigation</div>
+      <div>Cart</div>
+    </div>
+  );
+};
+
+export default Navbar;
