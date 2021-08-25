a4212533031b710deaa8367a17ec06ab3cdd41f8
J-Whisler <jwhisler521@gmail.com>
2021-08-25 18:55:36 -0400

    initial react-router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, BrowserRouter} from "react-router-dom"
 
 import Navbar from './Navbar/Navbar';
+import HomePage from './pages/HomePage';
+import CartPage from './pages/CartPage';
+import Cart from './Cart/Cart';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Navbar />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/cart" component={CartPage} />
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
