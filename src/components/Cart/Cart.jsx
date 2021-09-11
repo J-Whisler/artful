8d70ec7bbb6a6ffc3a4dd67515d6b2f8094052b3
J-Whisler <jwhisler521@gmail.com>
2021-09-11 17:28:21 -0400

    Cart empty message styling and button to shop page, more styling for cart card

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Cart.scss";
-
+import { Link } from "react-router-dom";
 import { useStateValue } from "../../context/StateProvider";
 import CartCard from "./CartCard/CartCard";
 
@@ -10,7 +10,15 @@ const Cart = () => {
   return (
     <div className="cart">
       {basket?.length === 0 ? (
-        <h2 className="cart__emptyMessage">Cart is Empty</h2>
+        <div className="cart__empty">
+          <h4 className="cart__emptyMessage">YOUR CART IS EMPTY!</h4>
+          <h6 className="cart__emptySecondMessage">
+            Click button below to see our full shop
+          </h6>
+          <Link className="cart__emptyButton" to="/shop">
+            See Full Shop
+          </Link>
+        </div>
       ) : (
         <div className="cart__items">
           {basket.map((item) => (
